cf258adde9c0e396377746db1abe9507da2a9763
Khushi Srivastava <khushisri1311@gmail.com>
2025-08-18 11:13:28 +0530

    components and router

--- NotesApp/src/App.jsx
+++ NotesApp/src/App.jsx
@@ -2,6 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Notes from './components/Notes'
+import ViewNotes from './components/ViewNotes'
+const router = createBrowserRouter(
+    [
+      {
+        path:"/",
+        element:
+        <div>
+          <Navbar/>
+          <Home/>
+        </div>
+      },
+      {
+        path:"/notes",
+        element:
+        <div>
+          <Navbar/>
+          <Notes/>
+        </div>
+      },
+      {
+        path:"/notes/:id",
+        element:
+        <div>
+          <Navbar/>
+          <ViewNotes/>
+        </div>
+      },
+    ]
+);
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +44,7 @@ function App() {
   return (
     <>
       <div>
+        <RouterProvider router={router}/>
         hello
       </div>
     </>
---
+++ NotesApp/src/components/Notes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notes = () => {
+  return (
+    <div>List of Notes</div>
+  )
+}
+
+export default Notes

0eac511579a105f0c2e9cfbcb5bd9c3aab2025df
Khushi Srivastava <khushisri1311@gmail.com>
2026-02-03 08:29:41 +0530

    navlinks and other functionalities added

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,28 @@ const express = require("express");
 const router = express.Router()
 const isLoggedin = require("../middlewares/isLoggedin");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/",(req,res) =>{
     let error = req.flash("error");
-    res.render("index",{error:''})
+    res.render("index",{error:'',isLoggedin:false})
 })
 router.get("/shop",isLoggedin,async (req,res) =>{
     let product = await productModel.find();
-    res.render("shop",{product})
+    let success = req.flash("success")
+    res.render("shop",{product,success})
+})
+router.get("/cart",isLoggedin,async (req,res) =>{
+    let user = await userModel.findOne({email:req.user.email}).populate("cart");
+    let bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount)
+    res.render("cart",{user,bill})
+})
+router.get("/addtocart/:productid",isLoggedin,async (req,res) =>{
+    let user = await userModel.findOne({email:req.user.email})
+    user.cart.push(req.params.productid)
+    await user.save();
+    req.flash("success","product added to cart successfully")
+    res.redirect("/shop")
 })
 router.get("/logout",isLoggedin,(req,res) =>{
     res.render("index")
